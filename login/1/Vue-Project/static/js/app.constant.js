3ae5ae3b2946d060573076d0310eb33e5076c8d2
zhangrui <1033956305@qq.com>
2018-05-10 15:47:20 +0800

    测试

--- login/1/Vue-Project/static/js/app.constant.js
+++ login/1/Vue-Project/static/js/app.constant.js
@@ -5,7 +5,4 @@ export const APP_CONSTANT = {
   API_HOST: 'http://101.200.85.207:8080/backstage',
   LOGINURL: 'http://101.200.85.207:8080/backstage/login.do',
   DETAILURL: 'http://101.200.85.207:8080/backstage/viewdetails.do'
-  // API_HOST: 'http://localhost:8080/backstage',
-  // LOGINURL: 'http://localhost:8080/backstage/login.do',
-  // DETAILURL: 'http://localhost:8080/backstage/viewdetails.do'
 }
